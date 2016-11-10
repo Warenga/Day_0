33c0c1355e23c62b4ee2466a6c657688440b8532
Warenga <warengam@gmail.com>
2016-11-10 19:39:42 +0300

    Adds a list comprehension to create a list of prime numbers generated

--- app/prime.py
+++ app/prime.py
@@ -9,25 +9,14 @@ def is_prime(num):
 		return False
 
 def prime_numbers(num):
-
-	prime = []
-
 	if not isinstance(num, int):
 		return "Only integers allowed"
 	elif num < 0:
 		return "Only Positive numbers allowed"
 	elif num == 2:
-		prime.append(2)
+		prime = [2]
 		return prime
 	else:
-		for i in range(2, num):
-			if is_prime(i):
-				prime.append(i)
+		prime = [i for i in range(2, num) if is_prime(i)]
 		return prime
-
-prime_numbers(10)
-
-
-
-
 
