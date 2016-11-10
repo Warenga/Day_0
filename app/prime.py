51e9d1abc0019b3224f80cd92a275de6a29842d7
Warenga <warengam@gmail.com>
2016-11-10 15:43:46 +0300

    Test for non integer input

---
+++ app/prime.py
@@ -0,0 +1,7 @@
+def prime_numbers(num):
+
+	result = []
+
+	if not isinstance(num, int):
+		return "Only integers allowed"
+	pass
--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
-import unittest
+from app import prime
 
-from . import prime_numbers.prime_numbers
+import unittest
 
 class PrimeNumbersTest(unittest.Testcase):
 
-	def test_validity(self):
-		self.assertEqual(prime_numbers(10), "Not a prime number")
+	# def test_validity(self):
+	# 	self.assertEqual(prime.prime_numbers(10), [2, 3, 5, 7])
 
+	def test_if_number_is_int(self):
+		self.assertEqual(prime_numbers("70"), "Only integers allowed")
